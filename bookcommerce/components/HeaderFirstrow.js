838492593002772f45ac727bd59d1e0616282bb9
Peter Amajor <93150831+thefrontendgod@users.noreply.github.com>
2022-05-06 13:53:46 +0100

    Update Iconset for mobile
    Update Iconset for mobile

--- bookcommerce/components/HeaderFirstrow.js
+++ bookcommerce/components/HeaderFirstrow.js
@@ -8,6 +8,7 @@ import {RiHandHeartLine} from 'react-icons/ri';
 import {BsShieldCheck} from 'react-icons/bs';
 import {AiOutlineDollar} from 'react-icons/ai';
 import {HiOutlineUsers} from 'react-icons/hi';
+import {ImMenu} from 'react-icons/im';
 
 // import styles
 import styles from '../styles/HeaderFirstrow.module.css'
@@ -39,6 +40,10 @@ export default function HeaderFirstrow() {
             <AiOutlineUser />
             <div className={styles.account_text}>Account</div>
             <MdOutlineKeyboardArrowDown className={styles.accountdrpdwn} />
+        </div>
+
+        <div className={`${styles.divwraps} ${styles.mobilehamburger}`}>
+            <ImMenu />
         </div>
     </div>
   )
